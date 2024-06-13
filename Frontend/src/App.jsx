1a64083deb25ec2ec7e9a49702ae399690ece16d
mishba-ai <mishbabagban@gmail.com>
2024-06-13 13:05:54 +0530

    create the landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,37 @@
-
+import Nomadlrs from "./assets/nomadlrs.png";
 
 function App() {
-
-
   return (
     <>
-       <header className=""> </header>
+      {/* landing section */}
+      <section className="w-screen h-screen bg-gradient-to-r from-black to-blue">
+        <header className="w-full h-16 ">
+          <ul className="flex justify-around font-bold text-xl font-mono pt-4 text-[rgb(86,97,199)]">
+            <li>
+              {" "}
+              <img
+                src={Nomadlrs}
+                alt="NomadX2"
+                className="absolute -top-20 left-2"
+              />
+            </li>
+            <li>Services</li>
+            <li>Destination</li>
+            <li>Pricing</li>
+            <li>
+              <button className="bg-[#5661C7] px-2 py-1 rounded-md text-[#FFC9F6]">
+                Go Nomad
+              </button>
+            </li>
+          </ul>
+        </header>
+
+        <section className="flex justify-center items-center">
+          <h1 className="text-6xl text-white">Nomad X</h1>
+        </section>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,7 +3,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "var(--blue)",
+      },
+    },
   },
   plugins: [],
 };
