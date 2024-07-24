9c4eb863282d121b059cf50e8f117f461705997e
mishba-ai <mishbabagban@gmail.com>
2024-07-24 17:37:41 +0530

    revamp the forum page

--- Frontend/src/Pages/Forum/Forum.jsx
+++ Frontend/src/Pages/Forum/Forum.jsx
@@ -2,8 +2,50 @@ import Header from "../../components/Forum/header";
 
 function Forum() {
   return (
-    <div className="w-full h-screen bg-[#fff]">
+    <div className="w-full h-screen  bg-[#fff] font-Robotomono">
       <Header />
+
+      <section className="px-48">
+        <nav className="">
+          <div className="flex justify-between mt-6 text-lg font-medium">
+            <ul className="flex gap-x-8">
+              <li className="">
+                <select className="rounded-xl py-1 border border-transparent focus:border-2 focus:border-[#98cdf9]">
+                  <option
+                    value=""
+                    selected
+                    hidden
+                    disabled
+                    className=" "
+                  >
+                    Categories
+                  </option>
+                  <option value="0">Travel Planning and Tips</option>
+                  <option value="1">Accommodation and Living</option>
+                  <option value="2">Safety and Health</option>
+                  <option value="3">Culture and Language</option>
+                  <option value="4">Technology and Work</option>
+                  <option value="5">Community and Social</option>
+                  <option value="6">Transportation</option>
+                  <option value="7">Photography and Videography</option>
+                </select>
+              </li>
+              <li><span className="cursor-pointer ">Latest</span></li>
+              <li>Top</li>
+            </ul>
+            <ul>
+              <li><button className="bg-[#98cdf9] p-2 px-3 rounded-lg text-sky-800 hover:bg-sky-300">New Topic</button></li>
+            </ul>
+          </div>
+        </nav>
+      </section>
+      
+      
+      <section className="px-44  justify-center flex  ">
+                <div className="text-4xl font-semibold w-96 text-sky-6">
+                  comming soon
+                </div>
+      </section>
     </div>
   );
 }
--- Frontend/src/components/Forum/header.jsx
+++ Frontend/src/components/Forum/header.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import search from "../../assets/svg/search.svg";
 
 function header() {
   return (
-    <div>
+    <div className=''>
 
-        <div className='w-full h-64 bg-gradient-to-r from-[#10161f] to-[#98cdf9]'>
-
+        <div className='w-full h-64 bg-gradient-to-r px-44  py-10  from-[#10161f] to-[#98cdf9]'>
+                <div className=' flex justify-center mt-28 '>
+                  <img src={search} alt=""className="w-6 h-6 relative left-10 top-3"/>
+                  <input type="text" className='w-[45rem] py-3 pr-4 pl-12 focus:border  focus:border-sky-200 rounded-full opacity-40' placeholder='Search'/>
+                </div>
         </div>
     </div>
   )
