fc63a2deb3919a689ec1687f3b945dbd778dc6e1
mishba-ai <mishbabagban@gmail.com>
2024-07-25 07:30:51 +0530

    revamp the landing page

--- Frontend/src/Pages/landing/Landing.jsx
+++ Frontend/src/Pages/landing/Landing.jsx
@@ -8,31 +8,35 @@ import Footer from "../../components/landing/Footer.jsx";
 function Landing() {
   return (
     <div>
-      <section className="overflow-x-hidden bg-gradient-to-r from-[#000] to-[#141b41]">
+      <section className="overflow-x-hidden font-Robotomono bg-gradient-to-r from-[#000] to-[#141b41]">
         {/* landing section */}
         <section className="w-screen h-screen  ">
           <Header />
 
           <div className=" ">
-            <h4 className="uppercase text-2xl text-[#fff]  absolute left-64 top-44  font-Robotomono">EXPlore your</h4>
+            <h4 className="uppercase text-2xl text-[#fff]  absolute left-52 top-44  font-mono text-opacity-30">Explore your</h4>
             <h1 className=" text-white justify-center flex font-Gagalin text-[20rem]">
               <span>W</span>
               <span>
                 <img
-                  src={oworld}
+                  src="https://upload.wikimedia.org/wikipedia/commons/7/7f/Rotating_earth_animated_transparent.gif"
                   alt="globe"
-                  className="h-[15rem] w-[20rem] relative mt-36  "
+                  className="h-[24rem] w-[24rem] relative mt-24 transition-transform scale-125 z-10  "
                   // onClick={() => setShowNewPage(true)}
                 />
-              </span>
-              <span>RLD</span>
+              </span>{""}
+              <span className="ml-8">RLD</span>
             </h1>
+          </div>
+
+          <div className="hidden">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/7/7f/Rotating_earth_animated_transparent.gif" alt="" />
           </div>
         </section>{" "}
         {/* this is for services */}
         {/* <section className="  h-screen w-screen "></section> */}
         {/* destination section */}
-        <section className="w-screen h-screen  overflow-y-hidden">
+        {/* <section className="w-screen h-screen  overflow-y-hidden">
           <ul className="flex ">
             <li>
               <img
@@ -50,8 +54,10 @@ function Landing() {
             <li></li>
             <li></li>
           </ul>
-        </section>
+        </section> */}
         {/* footer */}
+
+        
         <Footer />
       </section>
     </div>
--- Frontend/src/components/landing/Header.jsx
+++ Frontend/src/components/landing/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <>
     <header className="w-full h-16 px-10 ">
-       <ul className="flex justify-around font-montserrat pt-4 text-gray-400">
+       <ul className="flex justify-around font-Robotomono pt-4 text-gray-300 ">
             <li>
               {" "}
               <img
@@ -14,7 +14,7 @@ function Header() {
                 className="absolute -top-6 left-44  w-32"
               />
             </li>
-            <ul className="flex gap-x-8 text-sm font-medium">
+            <ul className="flex gap-x-8  mt-2 text-sm font-medium">
               <li className="hover:underline hover:decoration-white hover:underline-offset-4">
                 <a href=""> Services</a>
               </li>
@@ -25,8 +25,9 @@ function Header() {
                 <a href=""> Pricing</a>
               </li>
             </ul>
+            
           <Link to="/Signup"> <li>
-              <button className="bg-gradient-to-r  from-black to-[#322E83] font-medium px-5 py-2 rounded-3xl text-white text-sm hover:transition-transform hover:bg-gradient-to-r hover:from-slate-800 hover:to-slate-400 hover:scale-105">
+              <button className="bg-gradient-to-r  from-black to-[#322E83] font-medium px-5 py-2 rounded-3xl text-white text-sm hover:transition-transform hover:scale-105">
                 Go Nomad
               </button>
             </li></Link> 
