ffe172370c976c10740d49515cbf0421b9b7dc89
mishba-ai <mishbabagban@gmail.com>
2024-09-20 18:48:23 +0530

    Migrated backend from Express.js to Django

---
+++ Backend/api/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+# User Serializer means  that we are going to serialize the user model and convert it into JSON format so that we can send it to the frontend. 
+class UserSerializer(serializers.ModelSerializer):
+    # Meta class is used to define the fields that we want to serialize.
+    class Meta: 
+        model = User 
+        fields =["id","username","password"] 
+        extra_kwargs = {"password": {"write_only": True, "required": True}} # extra_kwargs is used to define the password field as write_only so that it is not visible to the user and required so that the user has to provide the password while registering.
+
+    # create method is used to create a new user.
+    def create(self,validated_data):
+        user = User.objects.create_user(**validated_data) # ** it means that we are passing the validated_data as a dictionary. dictionary means key value pair. 
+        return user 
